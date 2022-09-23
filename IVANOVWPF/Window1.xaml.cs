d2e00a4185209592ccc4be3b193220860f62a123
Семен Иванов <apt.semen2003@bk.ru>
2022-09-23 15:05:58 +1000

    .....

--- IVANOVWPF/Window1.xaml.cs
+++ IVANOVWPF/Window1.xaml.cs
@@ -10,30 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace IVANOVWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Win1_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 window1 = new Window1();
-            window1.Show();
-        }
-
-        private void Win2_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
